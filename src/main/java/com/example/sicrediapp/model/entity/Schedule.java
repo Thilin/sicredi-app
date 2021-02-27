cd954029124e020148dbb1e06c32333ed7dbaf36
Thilin <fernando_cauper@hotmail.com>
2021-02-27 16:32:28 -0400

    feat: transformed the class into a JPA entity

--- src/main/java/com/example/sicrediapp/model/entity/Schedule.java
+++ src/main/java/com/example/sicrediapp/model/entity/Schedule.java
@@ -2,13 +2,23 @@ package com.example.sicrediapp.model.entity;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
+@Table(name = "TB_SCHEDULE")
 public class Schedule {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "SCH_ID")
     private Long id;
+
+    @Column(name = "SCH_DURATION")
     private Long duration;
-    private Boolean isOpen;
+    @Column(name = "SCH_ISOPEN")
+    private boolean isOpen;
 }

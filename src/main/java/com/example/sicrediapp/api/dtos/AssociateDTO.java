36910a512be713a9648734201823fed665840ee0
Thilin <fernando_cauper@hotmail.com>
2021-02-27 19:04:51 -0400

    refactor: added some documentation to associates

--- src/main/java/com/example/sicrediapp/api/dtos/AssociateDTO.java
+++ src/main/java/com/example/sicrediapp/api/dtos/AssociateDTO.java
@@ -1,5 +1,6 @@
 package com.example.sicrediapp.api.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +12,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class AssociateDTO {
 
+    @Schema(description = "Associate's CPF", example = "01373891000", minimum = "11", maximum = "11")
     private String cpf;
+    @Schema(description = "Associate's Name", example = "Fernando")
     private String name;
 }
--- src/main/java/com/example/sicrediapp/api/dtos/AssociateListDTO.java
+++ src/main/java/com/example/sicrediapp/api/dtos/AssociateListDTO.java
@@ -1,5 +1,6 @@
 package com.example.sicrediapp.api.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Data
@@ -7,7 +8,10 @@ import lombok.*;
 @AllArgsConstructor
 public class AssociateListDTO {
 
+    @Schema(description = "Associate's id", example = "1")
     private Long id;
+    @Schema(description = "Associate's Name", example = "Fernando")
     private String name;
+    @Schema(description = "Associate's CPF", example = "01373891000", minimum = "11", maximum = "11")
     private String cpf;
 }

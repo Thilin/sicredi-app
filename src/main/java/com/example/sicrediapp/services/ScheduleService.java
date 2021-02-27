ab2f3976ea1de532121aeaa929966f091a9f7437
Thilin <fernando_cauper@hotmail.com>
2021-02-27 16:29:11 -0400

    refactor: if duration is less than 1, throws an exception

--- src/main/java/com/example/sicrediapp/services/ScheduleService.java
+++ src/main/java/com/example/sicrediapp/services/ScheduleService.java
@@ -1,6 +1,6 @@
 package com.example.sicrediapp.services;
 
-import com.example.sicrediapp.domains.Schedule;
+import com.example.sicrediapp.model.entity.Schedule;
 
 public interface ScheduleService {
     Schedule save(Schedule any);
--- src/main/java/com/example/sicrediapp/services/impl/ScheduleServiceImpl.java
+++ src/main/java/com/example/sicrediapp/services/impl/ScheduleServiceImpl.java
@@ -1,9 +1,9 @@
 package com.example.sicrediapp.services.impl;
 
-import com.example.sicrediapp.domains.Schedule;
-import com.example.sicrediapp.repositories.ScheduleRepository;
+import com.example.sicrediapp.model.entity.Schedule;
+import com.example.sicrediapp.api.exceptions.InvalidScheduleDurationException;
+import com.example.sicrediapp.model.repositories.ScheduleRepository;
 import com.example.sicrediapp.services.ScheduleService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,6 +17,9 @@ public class ScheduleServiceImpl implements ScheduleService {
 
     @Override
     public Schedule save(Schedule schedule) {
+        if(schedule.getDuration() < 1L)
+            throw new InvalidScheduleDurationException("The duration is less than 1 minute");
+
         return scheduleRepository.save(schedule);
     }
 }

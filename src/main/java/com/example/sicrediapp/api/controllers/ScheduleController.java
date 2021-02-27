09e33326eb38b61fc0f8b1c1c901df1b5f497888
Thilin <fernando_cauper@hotmail.com>
2021-02-27 16:31:11 -0400

    refactor: changed the exceptions response to another class

--- src/main/java/com/example/sicrediapp/api/controllers/ScheduleController.java
+++ src/main/java/com/example/sicrediapp/api/controllers/ScheduleController.java
@@ -1,18 +1,10 @@
-package com.example.sicrediapp.controllers;
+package com.example.sicrediapp.api.controllers;
 
-import com.example.sicrediapp.domains.Schedule;
-import com.example.sicrediapp.dtos.ScheduleDTO;
-import com.example.sicrediapp.exceptions.ApiErrors;
-import com.example.sicrediapp.exceptions.InvalidScheduleDurationException;
+import com.example.sicrediapp.model.entity.Schedule;
+import com.example.sicrediapp.api.dtos.ScheduleDTO;
 import com.example.sicrediapp.services.ScheduleService;
-import org.apache.coyote.Response;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -34,18 +26,5 @@ public class ScheduleController {
     public ScheduleDTO create(@Valid @RequestBody ScheduleDTO dto){
         var schedule = modelMapper.map(dto, Schedule.class);
         return modelMapper.map(scheduleService.save(schedule), ScheduleDTO.class);
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiErrors handleValidationException(MethodArgumentNotValidException ex){
-        BindingResult bindResult = ex.getBindingResult();
-        return new ApiErrors(bindResult);
-    }
-
-    @ExceptionHandler(InvalidScheduleDurationException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiErrors handleInvalidScheduleDurationException(InvalidScheduleDurationException ex){
-        return new ApiErrors(ex);
     }
 }

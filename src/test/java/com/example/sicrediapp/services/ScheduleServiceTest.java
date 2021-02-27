aa86a270ddc2b8f0d6c83e78156daba8a93a5ac5
Thilin <fernando_cauper@hotmail.com>
2021-02-27 16:28:07 -0400

    test: ensure to not create a schedule with less than 1 minute of duration within the service

--- src/test/java/com/example/sicrediapp/services/ScheduleServiceTest.java
+++ src/test/java/com/example/sicrediapp/services/ScheduleServiceTest.java
@@ -1,8 +1,10 @@
 package com.example.sicrediapp.services;
 
-import com.example.sicrediapp.domains.Schedule;
-import com.example.sicrediapp.repositories.ScheduleRepository;
+import com.example.sicrediapp.model.entity.Schedule;
+import com.example.sicrediapp.api.exceptions.InvalidScheduleDurationException;
+import com.example.sicrediapp.model.repositories.ScheduleRepository;
 import com.example.sicrediapp.services.impl.ScheduleServiceImpl;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -31,7 +33,7 @@ public class ScheduleServiceTest {
     @Test
     @DisplayName("Should create a new schedule")
     public void saveScheduleTest(){
-        var schedule = Schedule.builder().duration(1L).isOpen(false).build();
+        var schedule = buildValidSchedule();
         Mockito.when(scheduleRepository.save(schedule))
                 .thenReturn(Schedule.builder().id(1L).duration(1L).isOpen(false).build());
 
@@ -40,5 +42,21 @@ public class ScheduleServiceTest {
         assertThat(savedSchedule.getId()).isNotNull();
         assertThat(savedSchedule.getDuration()).isEqualTo(1L);
         assertThat(savedSchedule.isOpen()).isFalse();
+    }
+
+    @Test
+    @DisplayName("Should not be able to create a schedule with less than 1 minute of duration")
+    public void createScheduleWithInvalidDurationTest(){
+        var schedule = Schedule.builder().duration(0L).isOpen(false).build();
+        var exception = Assertions.catchThrowable(()-> scheduleService.save(schedule));
+
+        assertThat(exception).isInstanceOf(InvalidScheduleDurationException.class)
+                .hasMessage("The duration is less than 1 minute");
+
+        Mockito.verify(scheduleRepository,Mockito.never()).save(schedule);
+    }
+
+    private Schedule buildValidSchedule(){
+        return Schedule.builder().duration(1L).isOpen(false).build();
     }
 }

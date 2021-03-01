65764587dda9df20d642c688b30651d11ea39016
Thilin <fernando_cauper@hotmail.com>
2021-03-01 10:42:42 -0400

    feat: added a query to check if the associate exists by CPF

--- src/main/java/com/example/sicrediapp/model/repositories/AssociateRepository.java
+++ src/main/java/com/example/sicrediapp/model/repositories/AssociateRepository.java
@@ -4,4 +4,5 @@ import com.example.sicrediapp.model.entity.Associate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AssociateRepository extends JpaRepository<Associate, Long> {
+    boolean existsByCpf(String cpf);
 }

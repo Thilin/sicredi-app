27b643c6494b775ad5f546bd283f1a7422aa6ea3
Thilin <fernando_cauper@hotmail.com>
2021-03-01 05:37:12 -0400

    test: Ensure the schedule creation

--- src/test/java/com/example/sicrediapp/services/ScheduleServiceTest.java
+++ src/test/java/com/example/sicrediapp/services/ScheduleServiceTest.java
@@ -1,6 +1,13 @@
 package com.example.sicrediapp.services;
 
+import com.example.sicrediapp.api.dtos.ScheduleDTO;
+import com.example.sicrediapp.model.entity.Schedule;
+import com.example.sicrediapp.model.repositories.ScheduleRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -9,44 +16,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
 public class ScheduleServiceTest {
-//
-//    ScheduleService scheduleService;
-//
-//    @MockBean
-//    ScheduleRepository scheduleRepository;
-//
-//    @BeforeEach
-//    public void setUp(){
-//        this.scheduleService = new ScheduleServiceImpl(scheduleRepository);
-//    }
-//
-//    @Test
-//    @DisplayName("Should create a new schedule")
-//    public void saveScheduleTest(){
-//        var schedule = buildValidSchedule();
-//        Mockito.when(scheduleRepository.save(schedule))
-//                .thenReturn(Schedule.builder().id(1L).duration(1L).isOpen(false).build());
-//
-//        var savedSchedule = scheduleService.save(schedule);
-//
-//        assertThat(savedSchedule.getId()).isNotNull();
-//        assertThat(savedSchedule.getDuration()).isEqualTo(1L);
-//        assertThat(savedSchedule.isOpen()).isFalse();
-//    }
-//
-//    @Test
-//    @DisplayName("Should not be able to create a schedule with less than 1 minute of duration")
-//    public void createScheduleWithInvalidDurationTest(){
-//        var schedule = Schedule.builder().duration(0L).isOpen(false).build();
-//        var exception = Assertions.catchThrowable(()-> scheduleService.save(schedule));
-//
-//        assertThat(exception).isInstanceOf(InvalidScheduleDurationException.class)
-//                .hasMessage("The duration is less than 1 minute");
-//
-//        Mockito.verify(scheduleRepository,Mockito.never()).save(schedule);
-//    }
-//
-//    private Schedule buildValidSchedule(){
-//        return Schedule.builder().duration(1L).isOpen(false).build();
-//    }
+
+    @MockBean
+    ScheduleService scheduleService;
+
+    @MockBean
+    ScheduleRepository scheduleRepository;
+
+    @Test
+    @DisplayName("Should create a new schedule")
+    public void saveScheduleTest(){
+        var schedule = Schedule.builder().description("Dividendos").build();
+        var dto = ScheduleDTO.builder().description("Dividendos").build();
+        var savedSchedule = Schedule.builder().id(1L).description("Dividendos").build();
+        Mockito.when(scheduleRepository.save(schedule))
+                .thenReturn(savedSchedule);
+
+        scheduleService.save(dto);
+
+        assertThat(savedSchedule.getId()).isNotNull();
+        assertThat(savedSchedule.getDescription()).isEqualTo("Dividendos");
+    }
 }

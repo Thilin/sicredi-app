337399d9cab321277949b53e0d631453fd6b00b4
Thilin <fernando_cauper@hotmail.com>
2021-03-02 21:09:06 -0400

    refactor: added some configuration when creating the columns

--- src/main/java/com/example/sicrediapp/model/entity/Session.java
+++ src/main/java/com/example/sicrediapp/model/entity/Session.java
@@ -17,16 +17,16 @@ public class Session {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "SES_ID")
+    @Column(name = "SES_ID", , nullable = false)
     private Long id;
 
-    @Column(name = "SES_DURATION", columnDefinition = "bigint unsigned default 1")
+    @Column(name = "SES_DURATION", columnDefinition = "bigint unsigned default 1", nullable = false)
     private Long duration;
 
-    @Column(name = "SES_ISOPEN")
+    @Column(name = "SES_ISOPEN", columnDefinition = "tinyint default 0", nullable = false)
     private boolean isOpen;
 
     @OneToOne
-    @JoinColumn(name = "SES_SCH_ID", referencedColumnName = "SCH_ID")
+    @JoinColumn(name = "SES_SCH_ID", referencedColumnName = "SCH_ID", nullable = false)
     private Schedule schedule;
 }

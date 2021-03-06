9960b82f54823e45b7bad9b62854384a5ec2b1b8
Thilin <fernando_cauper@hotmail.com>
2021-03-05 22:12:56 -0400

    feat: added the exception handle

---
+++ src/main/java/com/example/sicrediapp/api/exceptions/ErrorHandlerException.java
@@ -0,0 +1,93 @@
+package com.example.sicrediapp.api.exceptions;
+
+import com.example.sicrediapp.model.entity.Session;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.OffsetDateTime;
+
+@RestControllerAdvice
+public class ErrorHandlerException extends ResponseEntityExceptionHandler {
+
+    public static final String MSG_ERRO_GENERICA_USUARIO_FINAL = "Ocorreu um erro interno no sistema. " +
+            "Tente novamente, se o problema persistir, entre em contato com o administrador.";
+
+    @Autowired
+    private MessageSource messageSource;
+
+    private Error.ErrorBuilder createErrorBuilder(HttpStatus status, ExceptionsEnum exception, String message){
+        return Error.builder().timestamp(OffsetDateTime.now()).status(status.value()).type(exception.getUri())
+                .title(exception.getDescription()).msg(message);
+    }
+
+    @ExceptionHandler(CountVoteSessionOpenException.class)
+    public ResponseEntity<Object> handleException(CountVoteSessionOpenException ex, WebRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        ExceptionsEnum errorType = ExceptionsEnum.COUNT_VOTE_SESSION_OPEN;
+        String msg = ex.getMessage();
+        Error errors = createErrorBuilder(status, errorType, msg).build();
+        return handleExceptionInternal(ex, errors, new HttpHeaders(), status, request);
+    }
+
+    @ExceptionHandler(DuplicateCPFException.class)
+    public ResponseEntity<Object> handleException(DuplicateCPFException ex, WebRequest request){
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        ExceptionsEnum errorType = ExceptionsEnum.DUPLICATE_CPF;
+        String msg = ex.getMessage();
+        Error errors = createErrorBuilder(status, errorType, msg).build();
+        return handleExceptionInternal(ex, errors, new HttpHeaders(), status, request);
+    }
+
+    @ExceptionHandler(ExternalServiceUnavailableException.class)
+    public ResponseEntity<Object> handleException(ExternalServiceUnavailableException ex, WebRequest request){
+        HttpStatus status = HttpStatus.SERVICE_UNAVAILABLE;
+        ExceptionsEnum errorType = ExceptionsEnum.EXTERNAL_SERVICE_UNAVAILABLE;
+        String msg = ex.getMessage();
+        Error errors = createErrorBuilder(status, errorType, msg).build();
+        return handleExceptionInternal(ex, errors, new HttpHeaders(), status, request);
+    }
+
+    @ExceptionHandler(InvalidSessionDurationException.class)
+    public ResponseEntity<Object> handleException(InvalidSessionDurationException ex, WebRequest request){
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        ExceptionsEnum errorType = ExceptionsEnum.INVALID_SESSION_DURATION;
+        String msg = ex.getMessage();
+        Error errors = createErrorBuilder(status, errorType, msg).build();
+        return handleExceptionInternal(ex, errors, new HttpHeaders(), status, request);
+    }
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<Object> handleException(ObjectNotFoundException ex, WebRequest request){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        ExceptionsEnum errorType = ExceptionsEnum.RESOURCE_NOT_FOUND;
+        String msg = ex.getMessage();
+        Error errors = createErrorBuilder(status, errorType, msg).build();
+        return handleExceptionInternal(ex, errors, new HttpHeaders(), status, request);
+    }
+
+    @ExceptionHandler(SessionClosedException.class)
+    public ResponseEntity<Object> handleException(SessionClosedException ex, WebRequest request){
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        ExceptionsEnum errorType = ExceptionsEnum.SESSION_CLOSED;
+        String msg = ex.getMessage();
+        Error errors = createErrorBuilder(status, errorType, msg).build();
+        return handleExceptionInternal(ex, errors, new HttpHeaders(), status, request);
+    }
+
+    @ExceptionHandler(UnableToVoteException.class)
+    public ResponseEntity<Object> handleException(UnableToVoteException ex, WebRequest request){
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        ExceptionsEnum errorType = ExceptionsEnum.UNABLE_TO_VOTE;
+        String msg = ex.getMessage();
+        Error errors = createErrorBuilder(status, errorType, msg).build();
+        return handleExceptionInternal(ex, errors, new HttpHeaders(), status, request);
+    }
+}
---
+++ src/main/java/com/example/sicrediapp/api/exceptions/ExceptionsEnum.java
@@ -0,0 +1,25 @@
+package com.example.sicrediapp.api.exceptions;
+
+import lombok.*;
+
+@Getter
+public enum ExceptionsEnum {
+
+
+    DUPLICATE_CPF("/cpf-duplicado", "O CPF já existe na base de dados"),
+    RESOURCE_NOT_FOUND("/recurso-nao-encontrado", "Recurso não encontrado"),
+    SESSION_CLOSED("/sessao-fechada", "Não é possível votar em uma sessão fechada"),
+    DUPLICATE_VOTE_SAME_SESSION("/voto-duplicado-mesma-sessao", "Um associado não pode votar mais de uma vez numa mesma sessão"),
+    EXTERNAL_SERVICE_UNAVAILABLE("/servico-externo-offline", "Nada retornou do serviço. Verificar com o provedor"),
+    UNABLE_TO_VOTE("/nao-pode-votar", "Associado não pode votar"),
+    INVALID_SESSION_DURATION("/duracao-sessao-invalida", "A duração da sessão não pode ser menor que 1 minuto"),
+    COUNT_VOTE_SESSION_OPEN("/votacao-sessao-aberta", "Não é possível ter o resultado da votação durante uma sessão aberta");
+
+
+    private String uri;
+    private String description;
+    ExceptionsEnum(String path, String description){
+        this.uri = "sicredi-app"+path;
+        this.description = description;
+    }
+}
